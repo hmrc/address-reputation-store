c9111d0126121e86d4539760f93ef1b12734f365
Rick <rickb777@github.com>
2016-11-23 10:51:13 +0000

    TXMNT-376 imported code from the ingester

---
+++ src/main/scala/uk/gov/hmrc/address/services/es/IndexName.scala
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2016 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.address.services.es
+
+import uk.gov.hmrc.util._
+import org.joda.time.DateTime
+import org.joda.time.format.DateTimeFormat
+
+// Normal collections will have all fields defined.
+// However, we allow for absent fields primarily for backward compatibility.
+
+trait IndexState {
+  def productName: String
+  def epoch: Option[Int]
+  def timestamp: Option[String]
+}
+
+
+case class IndexName(productName: String,
+                     epoch: Option[Int],
+                     timestamp: Option[String] = None) extends Ordered[IndexName] with IndexState {
+
+  def toPrefix: String = s"${productName}_${epoch.get}"
+
+  override lazy val toString: String =
+    if (timestamp.isDefined) IndexName.format(productName, epoch.get, timestamp.get)
+    else if (epoch.isDefined) toPrefix
+    else productName
+
+  override def compare(that: IndexName): Int = this.toString compare that.toString
+}
+
+
+object IndexName {
+  def apply(collectionName: String): Option[IndexName] =
+    if (collectionName.isEmpty) None
+    else {
+      val parts = qsplit(collectionName, '_')
+      if (parts.size <= 3) doParseName(parts) else None
+    }
+
+  private def doParseName(parts: List[String]): Option[IndexName] = {
+    try {
+      val epoch = if (parts.size >= 2) Some(parts(1).toInt) else None
+      val timestamp = if (parts.size >= 3) Some(parts(2)) else None
+      Some(IndexName(parts.head, epoch, timestamp))
+    } catch {
+      case n: NumberFormatException => None
+    }
+  }
+
+  def format(productName: String, epoch: Int, timestamp: String): String = "%s_%d_%s".format(productName, epoch, timestamp)
+
+  def newTimestamp = timestampFormatter.print(new DateTime())
+
+  // note: no underscores (would break our logic) and no dashes (they are problematic in Mongo)
+  val timestampFormatter = DateTimeFormat.forPattern("yyyyMMddHHmm")
+}
---
+++ src/test/scala/uk/gov/hmrc/address/services/es/IndexNameTest.scala
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2016 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.address.services.es
+
+import org.scalatest.FunSuite
+
+class IndexNameTest extends FunSuite {
+
+  test("format IndexName") {
+    assert(IndexName("fooey", Some(1), Some("ts2")).toString === "fooey_1_ts2")
+    assert(IndexName("fooey", Some(40), Some("ts13")).toString === "fooey_40_ts13")
+    assert(IndexName("fooey", Some(40), None).toString === "fooey_40")
+    assert(IndexName("fooey", None, None).toString === "fooey")
+  }
+
+  test("IndexName prefix") {
+    assert(IndexName("fooey", Some(1), Some("ts2")).toPrefix === "fooey_1")
+    assert(IndexName("fooey", Some(40), None).toPrefix === "fooey_40")
+  }
+
+  test("parse via apply") {
+    assert(IndexName("") === None)
+    assert(IndexName("foo") === Some(IndexName("foo", None, None)))
+    assert(IndexName("foo_bar") === None)
+    assert(IndexName("foo_bar_baz") === None)
+    assert(IndexName("abp_40") === Some(IndexName("abp", Some(40), None)))
+    assert(IndexName("abp_40_ts2") === Some(IndexName("abp", Some(40), Some("ts2"))))
+    assert(IndexName("abp_40_ts2_zz") === None)
+    assert(IndexName("abp_foo_ts2") === None)
+  }
+}
